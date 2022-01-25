d8b62565af3c481b49e1bb1e033b9b5bd81248e4
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2022-01-25 20:55:49 +0700

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/ilmanhdyt/ShiraoriBOT.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/Uhdahlah/wabot-md.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, isROwner }) => {
 handler.help = ['update']
 handler.tags = ['owner']
 handler.command = /^update$/i
-handler.owner = true
+handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
